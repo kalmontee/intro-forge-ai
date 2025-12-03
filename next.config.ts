7955d56ea6636f4a33e17d3aaed648000345b623
Kelvin Almonte <kelvin.almonte95@gmail.com>
2025-12-02 19:18:18 -0500

    Update next.config.ts to change target from 'serverless' to 'export' for improved deployment compatibility

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  target: 'serverless',
+  output: 'export',
   basePath: process.env.PAGES_BASE_PATH,
 };
 
